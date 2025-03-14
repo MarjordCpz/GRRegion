d451fbbec0652300fcd756f729ba252f1b2e179c
MarjordCpz <3192162843@qq.com>
2025-03-14 19:04:25 +0800

    submit tasks bash

---
+++ render_bev.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#SBATCH --partition=gpu_4090  
+#SBATCH --nodes=1             
+#SBATCH --gres=gpu:2          
+#SBATCH --cpus-per-task=12     
+#SBATCH --time=14-00:00:00    
+#SBATCH --array=0-3
+#SBATCH --output=/ailab/user/caopeizhou/projects/GRRegion/logs/117/%j.out       
+#SBATCH --error=/ailab/user/caopeizhou/projects/GRRegion/logs/117/%j.err 
+
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 part_index usd_fodler"
+    exit 1
+fi
+part_index=$1
+usd_fodler=$2
+
+
+task_id=$SLURM_ARRAY_TASK_ID
+
+start_id=$((task_id*25))
+end_id=$(((task_id+1)*25))
+
+echo ${start_id}
+echo ${end_id}
+
+for ((i = start_index; i < end_index; i += 1)); do
+    /ailab/user/caopeizhou/apps/isaac-sim-4.1.0/python.sh getmaps.py \
+        --scene_id $((i)) \
+        --part $part_index \
+        --usd_folder $usd_fodler
+    wait
+done
